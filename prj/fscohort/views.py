58259285f9c9a5160db0473138374aedfe1712e2
Murat Sahin <yazilimogren1111@gmail.com>
2020-12-26 17:08:37 +0100

    template is added

--- prj/fscohort/views.py
+++ prj/fscohort/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home_view(request):
-    return HttpResponse('That is a message that i have own created on views.py!')
 
 def about_view(request):
     return HttpResponse('about')
+
+def home_view(request):
+    # print(request.GET.get("q"))
+    # print(request.COOKIES)
+    # print(request.user)
+    # print(request.path)
+    # print(request.method)
+    # return HttpResponse("Hi, this is fscohort Home page.")
+    my_context = {
+        "title": "clarusway",
+        "dict_1": {"django": "best framwort"},
+        "my_list": [1, 2, 3, 4]
+    }
+    return render(request, "fscohort/home.html", my_context)
+
+
+
